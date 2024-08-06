1a0a2a39fd82eb2a016c7975b05b7267cb7ca098
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-08-06 13:58:40 -0400

    update

--- Binning/RUNME_GTDBtk.bash
+++ Binning/RUNME_GTDBtk.bash
@@ -43,7 +43,7 @@ fi ;
 cd $dir
 
 # Get a list of subdirectories within the main directory
-sub_directories=($(find "15.metabat2" -mindepth 1 -maxdepth 1 -type d))
+sub_directories=($(find "17.checkm" -mindepth 1 -maxdepth 1 -type d))
 
 # Loop through each sub-directory
 for subdir in "${sub_directories[@]}"; do
@@ -51,19 +51,15 @@ for subdir in "${sub_directories[@]}"; do
    b=$(basename $subdir)
    OPTS="SAMPLE=$b,FOLDER=$dir"
    
-   mkdir $dir/17.checkm/$b
-   
    # Move into the sub-directory
-   cd "$dir/15.metabat2/$b" || exit
+   cd "$dir/17.checkm/$b" || exit
    
+   # Launch jobs
+   sbatch -v "$OPTS" -N "GTDBTK" -R y -pe multicore 60 -q $QUEUE $pac/run3.pbs | grep .;
+
    sbatch --export="$OPTS" -J "CheckM-$b" --account=$QUEUE --partition=$QOS --error "$dir"/"CheckM-$b"-%j.err -o "$dir"/"CheckM-$b"-%j.out  $pac/run_CheckM.pbs | grep .;
 
 done
 
 
 
-
-# Launch jobs
-
-OPTS="SAMPLE=$b,FOLDER=$dir"
-qsub -v "$OPTS" -N "GTDBTK" -R y -pe multicore 60 -q $QUEUE $pac/run3.pbs | grep .
