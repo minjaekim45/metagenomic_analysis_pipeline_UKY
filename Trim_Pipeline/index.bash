c58c145ac6668353fcb8657938fdc0cb89e77b63
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-08-27 13:08:57 -0400

    Update index.bash

--- Trim_Pipeline/index.bash
+++ Trim_Pipeline/index.bash
@@ -7,7 +7,7 @@
 #SBATCH -o index-%j.out             # Output file for this job.
 #SBATCH --account=coa_mki314_uksr     # Project allocation account name (REQUIRED)
 
-cd /scratch/jwme229/host_contam_rmvl/human_genome ;
+cd /scratch/jwme229/host_contam_rmvl ;
 
 source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
 
@@ -15,7 +15,7 @@ conda activate hocort
 
 mkdir GRCh38_p14 ;
 
-hocort index bowtie2 --input GCF_000001405.40_GRCh38.p14_genomic.fasta --output /scratch/jwme229/host_contam_rmv/human_genome/GRCh38_p14 ;
+hocort index bowtie2 --input GCF_000001405.40_GRCh38.p14_genomic.fasta --output /scratch/jwme229/host_contam_rmv/GRCh38_p14 ;
 
 conda deactivate
 
