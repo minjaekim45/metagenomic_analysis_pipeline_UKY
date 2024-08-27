0cb968eb160a9768ac69e681acfd36493df92bfa
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-08-26 20:10:45 -0400

    Update index.bash

--- Trim_Pipeline/index.bash
+++ Trim_Pipeline/index.bash
@@ -3,8 +3,8 @@
 #SBATCH --job-name=index            # Job name
 #SBATCH --ntasks=4                  # Number of cores for the job. Same as SBATCH -n 1
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
-#SBATCH -e unzip-%j.err             # Error file for this job.
-#SBATCH -o unzip-%j.out             # Output file for this job.
+#SBATCH -e index-%j.err             # Error file for this job.
+#SBATCH -o index-%j.out             # Output file for this job.
 #SBATCH --account=coa_mki314_uksr     # Project allocation account name (REQUIRED)
 
 cd /scratch/jwme229/host_contam_rmvl/human_genome
