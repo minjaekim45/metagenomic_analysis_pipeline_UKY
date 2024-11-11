5bffee45f26f7a3b46047ddcf420e48a0f1bd2fb
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-11-11 18:26:17 -0500

    Update RUNME.bash

--- Trim_Pipeline/RUNME.bash
+++ Trim_Pipeline/RUNME.bash
@@ -2,7 +2,7 @@
 
 if [[ "$1" == "" || "$1" == "-h" ]] ; then
    echo "
-   Usage: ./RUNME.bash folder filtration queue QOS
+   Usage: ./RUNME.bash [folder] [filtration] [queue] [QOS]
 
    folder	Path to the folder containing the raw reads. The raw reads must be in FastQ format,
    		and filenames must follow the format: <name>.<sis>.fastq, where <name> is the name
