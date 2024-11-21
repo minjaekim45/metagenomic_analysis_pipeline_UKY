e18f18678d36b6fdcccd2aae051aa53fea909aa9
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-11-21 11:37:42 -0500

    Update stats.bash

--- Trim_Pipeline/stats.bash
+++ Trim_Pipeline/stats.bash
@@ -2,7 +2,7 @@
 
 #SBATCH --time=06:00:00             # Time limit for the job (REQUIRED).
 #SBATCH --job-name=stats            # Job name
-#SBATCH --ntasks=4                  # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --ntasks=8                  # Number of cores for the job. Same as SBATCH -n 1
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
 #SBATCH -e stats-%j.err             # Error file for this job.
 #SBATCH -o stats-%j.out             # Output file for this job.
@@ -12,7 +12,7 @@ if [[ "$1" == "" || "$1" == "-h" ]] ; then
    echo "
    Usage: sbatch ./stats.bash [folder]
 
-   folder      Path to the folder containing the 'trim_output' directory.
+   folder      Path to the folder containing the '01.raw_reads' and '02.trimmed_reads' directories.
    
    " >&2 ;
    exit 1 ;
@@ -21,18 +21,28 @@ fi ;
 dir=$(readlink -f $1) ;
 
 #---------------------------------------------------------
-# Pull statistics from .err files
 
 cd $dir ;
-output_file="statistics.txt"
+> "$dir"/stats.txt
 
-for i in $dir/trim_output/*.err ; do 
-   sed -n "73,82p" "$i" >> "$output_file"
-   sed -n "301,310p" "$i" >> "$output_file"
-   sed -n "545,562p" "$i" >> "$output_file"
-   echo -n "," >> "$output_file"
+for i in $dir/01.raw_reads/*.fastq ; do
+   b1=$(basename $i .fastq) ;
+   line_count_1=$(wc -l "$i") ;
+   echo "$b1: $line_count_1;" >> "stats.txt"
 done ;
 
-echo "Done: $(date)."
+for i in $dir/02.trimmed_reads/*_val_*.fq ; do
+   b2=$(basename $i .fq) ;
+   line_count_2=$(wc -l "$i") ;
+   echo "$b2: $line_count_2;" >> "stats.txt"
+done ;
 
+for i in $dir/02.trimmed_reads/*.filtered_*.fq ; do
+   b3=$(basename $i .fq) ;
+   line_count_3=$(wc -l "$i") ;
+   echo "$b3: $line_count_3;" >> "stats.txt"
+done ;
 
+#---------------------------------------------------------
+
+echo "Done: $(date)." ;
