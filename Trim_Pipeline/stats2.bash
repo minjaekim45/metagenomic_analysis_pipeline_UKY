f9aab99efd3bf5b50eaefe20dd259e8fe7b1c1ef
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-01-17 08:20:17 -0500

    Create stats2.bash

---
+++ Trim_Pipeline/stats2.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --time=06:00:00             # Time limit for the job (REQUIRED).
+#SBATCH --job-name=stats            # Job name
+#SBATCH --ntasks=8                  # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e stats-%j.err             # Error file for this job.
+#SBATCH -o stats-%j.out             # Output file for this job.
+#SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
+
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   Usage: sbatch ./stats.bash [folder]
+
+   folder      Path to the folder containing the '01.raw_reads' and '02.trimmed_reads' directories.
+   
+   " >&2 ;
+   exit 1 ;
+fi ;
+
+dir=$(readlink -f $1) ;
+
+#---------------------------------------------------------
+
+cd $dir/01.raw_reads ;
+
+./sequence-stats/src/sequence-stats -s .fastq > 01.stats.bash
