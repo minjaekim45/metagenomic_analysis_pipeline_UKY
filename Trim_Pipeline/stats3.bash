41e2eb81b196619361fb342cb85a03ed67f1608c
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-01-20 20:26:00 -0500

    Update stats3.bash

--- Trim_Pipeline/stats3.bash
+++ Trim_Pipeline/stats3.bash
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+#SBATCH --time=06:00:00             # Time limit for the job (REQUIRED).
+#SBATCH --job-name=stats            # Job name
+#SBATCH --ntasks=8                  # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e stats-%j.err             # Error file for this job.
+#SBATCH -o stats-%j.out             # Output file for this job.
+#SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
+
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   Usage: sbatch ./stats.bash [folder]
+
+   folder      Path to the folder containing the '01.raw_reads' and '02.trimmed_reads' directories.
+   
+   " >&2 ;
+   exit 1 ;
+fi ;
+
+dir=$(readlink -f $1) ;
+cd $dir ;
+
+#---------------------------------------------------------
+
 # Directory containing the .fastq files
-directory="./"  # You can specify the directory path if needed
+dir1="$dir"/01.raw_reads ;
 
 # Output file
 output_file="statistics_output.txt"
@@ -10,7 +33,7 @@ output_file="statistics_output.txt"
 echo -e "File\tSequence_ID\tRead_Length\tContigs\tReads_G\tReads_C\tReads_A\tReads_T\tN50" > "$output_file"
 
 # Iterate over each .fastq file in the directory
-for file in "$directory"/*.fastq; do
+for file in "$dir1"/*.fastq; do
   # Check if the file exists
   if [ -f "$file" ]; then
     # Initialize counters and variables
