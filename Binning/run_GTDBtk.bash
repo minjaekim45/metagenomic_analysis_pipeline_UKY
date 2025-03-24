f140546f041954aedd08c7af27b328ba3889b338
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-03-24 18:55:49 -0400

    Update run_GTDBtk.bash

--- Binning/run_GTDBtk.bash
+++ Binning/run_GTDBtk.bash
@@ -54,7 +54,7 @@ database=/share/examples/MCC/gtdbtk/release220:/gtdbtk_data/database
 
 echo "==[ 17.checkm: $(date) ]"
 
-cd $FOLDER/17.checkm/output/good_quality
+cd $dir/17.checkm/output/good_quality
 
 readlink -f *.fa > list.txt
 
@@ -62,9 +62,9 @@ cat list.txt |  awk '{print $NF}' FS=/ | sed -e 's/\.fasta$//' > ID.txt
 
 paste list.txt ID.txt > batchfile.txt
 
-singularity run --env GTDBTK_DATA_PATH=/gtdbtk_data/database -B $database --app gtdbtk240 $container gtdbtk classify_wf --batchfile $FOLDER/17.checkm/output/good_quality/batchfile.txt --out_dir $FOLDER/18.gtdbtk/ --mash_db ../09.mash/mash.all.msh --extension fa --cpus $THR
+singularity run --env GTDBTK_DATA_PATH=/gtdbtk_data/database -B $database --app gtdbtk240 $container gtdbtk classify_wf --batchfile $dir/17.checkm/output/good_quality/batchfile.txt --out_dir $dir/18.gtdbtk/ --mash_db ../09.mash/mash.all.msh --extension fa --cpus $THR
 
-cd $FOLDER/18.gtdbtk/
+cd $dir/18.gtdbtk/
 
 #----------------------------------------------------------
 echo "Done: $(date)."
