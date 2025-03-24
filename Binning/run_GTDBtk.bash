e965a295dab5fad69a63147b14f555fd49caae44
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-03-24 18:35:41 -0400

    Create run_GTDBtk.bash

---
+++ Binning/run_GTDBtk.bash
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#SBATCH --time=06:00:00             # Time limit for the job (REQUIRED).
+#SBATCH --job-name=index            # Job name
+#SBATCH --ntasks=48                 # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e index-%j.err             # Error file for this job.
+#SBATCH -o index-%j.out             # Output file for this job.
+#SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
+
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   Usage: sbatch ./index.bash [folder] [genome] [accession]
+
+   folder      Path to the folder containing the compressed genome dataset from NCBI.
+               Compressed file should follow the format of 'human_GRCh38_dataset.zip',
+               though you may use a more recent version.
+   
+   genome      Name of the genome assembly. Defaults to 'GRCh38_p14' to represent the
+               most recent human genome dataset.
+   
+   accession   NCBI accession number. Defaults to 'GCF_000001405.40' to represent the
+               most recent human genome dataset.
+   
+   " >&2 ;
+   exit 1 ;
+fi ;
+
+dir=$(readlink -f $1) ;
+cd $dir
+
+if [[ ! -e 17.checkm ]] ; then
+   echo "Cannot locate the 17.checkm directory, aborting..." >&2
+   exit 1
+fi ;
+
+for i in 18.gtdbtk; do
+   [[ -d $i ]] || mkdir $i
+done
+
+#---------------------------------------------------------
+
+enve=/project/mki314_uksr/enveomics/Scripts
+THR=48
+
+source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
+
+# Container and database paths remain the same for anyone running on the MCC cluster
+container=/share/singularity/images/ccs/conda/amd-conda15-rocky8.sinf
+database=/share/examples/MCC/gtdbtk/release220:/gtdbtk_data/database
+
+#---------------------------------------------------------
+# Run GTDBtk
+
+echo "==[ 17.checkm: $(date) ]"
+
+cd $FOLDER/17.checkm/output/good_quality
+
+readlink -f *.fa > list.txt
+
+cat list.txt |  awk '{print $NF}' FS=/ | sed -e 's/\.fasta$//' > ID.txt
+
+paste list.txt ID.txt > batchfile.txt
+
+singularity run --env GTDBTK_DATA_PATH=/gtdbtk_data/database -B $database --app gtdbtk240 $container gtdbtk classify_wf --batchfile $FOLDER/17.checkm/output/good_quality/batchfile.txt --out_dir $FOLDER/18.gtdbtk/ --mash_db ../09.mash/mash.all.msh --extension fa --cpus $THR
+
+cd $FOLDER/18.gtdbtk/
+
+#----------------------------------------------------------
+echo "Done: $(date)."
+
+
+
+
+
