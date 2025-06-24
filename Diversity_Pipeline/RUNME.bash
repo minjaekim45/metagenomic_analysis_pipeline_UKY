ec9734fee7c7e6b7a1f8c459774bc22171052c53
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-06-24 16:02:14 -0400

    Update RUNME.bash

--- Diversity_Pipeline/RUNME.bash
+++ Diversity_Pipeline/RUNME.bash
@@ -3,19 +3,18 @@
 if [[ "$1" == "" || "$1" == "-h" ]] ; then
    echo "
    Usage: ./RUNME.bash [folder] [queue] [QOS]
-
-   folder	Path to the folder containing the 04.trimmed_fasta folder. The
-		trimmed reads must be in interposed FastA format, and filenames
-		must follow the format: <name>.CoupledReads.fa, where <name> is
-		the name of the sample. If non-paired, the filenames must follow
-		the format: <name>.SingleReads.fa. If both suffixes are found
-		for the same <name> prefix, they are both used.
-   partition	select a partition (if no provided, smem will be used)
-   qos			select a quality of service (if no provided, normal will be used)
    
-   Nonpareil: alignment option will be used. Otherwise, default.
-   Mash: k-mer size will be 25. (Mash's default is 21).
-         sketch size will be 100000. (Mash's default is 1000).
+   folder      Path to the folder containing the '04.trimmed_fasta' directory, where the trimmed reads
+               are stored. The trimmed reads must be in interposed FastA format, and filenames must
+	       follow the format: <name>.CoupledReads.fa, where <name> is the name of the sample. If
+	       non-paired, the filenames must follow the format: <name>.SingleReads.fa. If both suffixes
+               are found for the same <name> prefix, they are both used.
+   partition   Select a partition (if not provided, coa_mki314_uksr will be used)
+   qos         Select a quality of service (if not provided, normal will be used)
+   #-----------------------------------------------------------------------------------------------------
+   Nonpareil:  Alignment option will be used. Otherwise, default.
+   Mash:       k-mer size will be 25. (Mash's default is 21).
+               Sketch size will be 100000. (Mash's default is 1000).
    Please see the references for these two methods before you run this pipeline.
    To change any options for nonpareil and mash, please modify your commands in run.pbs.
 
@@ -37,6 +36,8 @@ dir=$(readlink -f $1)
 pac=$(dirname $(readlink -f $0))
 cwd=$(pwd)
 
+#---------------------------------------------------------
+
 cd $dir
 if [[ ! -e 04.trimmed_fasta ]] ; then
    echo "Cannot locate the 04.trimmed_fasta directory, aborting..." >&2
@@ -52,8 +53,6 @@ for i in $dir/04.trimmed_fasta/*.SingleReads.fa ; do
    touch $dir/04.trimmed_fasta/$b.CoupledReads.fa
 done
 
-# Launch jobs
-
 for i in $dir/04.trimmed_fasta/*.CoupledReads.fa ; do
    b=$(basename $i .CoupledReads.fa)
    OPTS="SAMPLE=$b,FOLDER=$dir"
@@ -65,3 +64,7 @@ for i in $dir/04.trimmed_fasta/*.CoupledReads.fa ; do
    # Launch job
    sbatch --export="$OPTS" -J "Diversity-$b" --account=$QUEUE --partition=$QOS --error "$dir"/zz.out/"Diversity-$b"-%j.err -o "$dir"/zz.out/"Diversity-$b"-%j.out  $pac/run.pbs | grep .;
 done 
+
+#---------------------------------------------------------
+
+echo "Done: $(date)." ;
