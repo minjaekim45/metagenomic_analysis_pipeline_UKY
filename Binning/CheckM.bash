cdef03f360ba02b24e4c4cba6f24dd35849ce8fe
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-06-26 18:07:22 -0400

    Update and rename run_CheckM.bash to CheckM.bash

--- Binning/CheckM.bash
+++ Binning/CheckM.bash
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #SBATCH --time=12:00:00             # Time limit for the job (REQUIRED).
-#SBATCH --job-name=checkm2           # Job name
+#SBATCH --job-name=CheckM2          # Job name
 #SBATCH --ntasks=20                 # Number of cores for the job. Same as SBATCH -n 1
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
-#SBATCH -e CheckM2-%j.err            # Error file for this job.
-#SBATCH -o CheckM2-%j.out            # Output file for this job.
+#SBATCH -e ./zz.out/CheckM2-%j.err  # Error file for this job.
+#SBATCH -o ./zz.out/CheckM2-%j.out  # Output file for this job.
 #SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
 
 if [[ "$1" == "" || "$1" == "-h" ]] ; then
    echo "
-   Usage: sbatch ./run_CheckM.bash [folder]
+   Usage: sbatch ./CheckM.bash [folder]
 
-   folder      Path to the folder containing 15.metabat2   
+   folder      Path to the folder containing the '15.metabat2' directory.   
    
    " >&2 ;
    exit 1 ;
 fi ;
 
 dir=$(readlink -f $1) ;
-cd $dir
+cd $dir ;
 
 if [[ ! -e 15.metabat2 ]] ; then
    echo "Cannot locate the 15.metabat2 directory, aborting..." >&2
@@ -32,25 +32,30 @@ done
 
 #---------------------------------------------------------
 
+# Change enveomics path to yours
 enve=/project/mki314_uksr/enveomics/Scripts
+
+# Source path to Conda environments
+source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
+
+# Database path remains the same for anyone on the MCC Cluster
 DB_path=/share/examples/MCC/checkm2_database/CheckM2_database/uniref100.KO.1.dmnd
+
+# The number of CPUs or threads
 THR=20
 
-source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
-conda activate checkm2
-#checkm2=/project/mki314_uksr/Software/checkm2/bin/checkm2
-
 #---------------------------------------------------------
+# Run CheckM2
 
-mkdir $dir/16.checkm2/output
+conda activate checkm2
 
-cd $dir/15.metabat2/binned
+mkdir $dir/16.checkm2/output ;
 
-#$checkm2 predict --threads $THR -x fa --tab_table --input . --output-directory $dir/16.checkm2
+cd $dir/15.metabat2/binned ;
 
 checkm2 predict -t $THR -x fa --input . --output-directory $dir/16.checkm2/output --database_path $DB_path
 
-cd $dir/16.checkm2
+cd $dir/16.checkm2 ;
 
 awk -F "\t" '{x=$2; y=$3 * 5} {if (x - y >= 50) print $0}' ./output/quality_report.tsv > ./output/high_qual.tsv
 
@@ -71,4 +76,5 @@ awk '{print $0, "$b"}' > output.txt
 conda deactivate
 
 #---------------------------------------------------------
+
 echo "Done: $(date)."
