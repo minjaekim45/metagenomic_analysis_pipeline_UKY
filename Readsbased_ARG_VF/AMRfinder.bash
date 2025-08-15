11490c32dbe468f019ea023208440ed8e14b53f8
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-08-15 15:18:37 -0400

    Update AMRfinder.bash

--- Readsbased_ARG_VF/AMRfinder.bash
+++ Readsbased_ARG_VF/AMRfinder.bash
@@ -2,10 +2,10 @@
 
 #SBATCH --time=12:00:00             # Time limit for the job (REQUIRED).
 #SBATCH --job-name=amr              # Job name
-#SBATCH --ntasks=8                  # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --ntasks=32                 # Number of cores for the job. Same as SBATCH -n 1
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
-#SBATCH -e /zz.out/amr-%j.err       # Error file for this job.
-#SBATCH -o /zz.out/amr-%j.out       # Output file for this job.
+#SBATCH -e ./zz.out/amr-%j.err      # Error file for this job.
+#SBATCH -o ./zz.out/amr-%j.out      # Output file for this job.
 #SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
 
 if [[ "$1" == "" || "$1" == "-h" ]] ; then
@@ -20,6 +20,10 @@ fi ;
 
 dir=$(readlink -f $1) ;
 
+for i in 20.amr_finder ; do
+   [[ -d $i ]] || mkdir $i
+done
+
 # Change database path to yours
 database=
 
@@ -27,7 +31,7 @@ database=
 source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
 
 # The number of CPUs or threads
-THR=8
+THR=32
 
 #---------------------------------------------------------
 # Run AMRfinder
@@ -38,7 +42,8 @@ cd $dir/20.amr_finder ;
 conda activate amr-finder
 
 for i in $dir/16.checkm2/output/good_quality/*.fa ; do
-amrfinder -p test_prot.fa -g test_prot.gff -n test_dna.fa -O Escherichia --plus
+   name=$(basename "$i" .fa)
+   amrfinder -p test_prot.fa -g test_prot.gff -n test_dna.fa -O Escherichia --plus --database <database_dir> --name <identifier>
 done ;
 
 conda deactivate
