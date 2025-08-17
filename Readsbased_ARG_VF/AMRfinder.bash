361bf66cc5ae365fc7a87b450270861e0b5a875e
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-08-17 16:43:37 -0400

    Update AMRfinder.bash

--- Readsbased_ARG_VF/AMRfinder.bash
+++ Readsbased_ARG_VF/AMRfinder.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --time=12:00:00             # Time limit for the job (REQUIRED).
-#SBATCH --job-name=amr              # Job name
-#SBATCH --ntasks=32                 # Number of cores for the job. Same as SBATCH -n 1
+#SBATCH --job-name=bakta            # Job name
+#SBATCH --ntasks=32                 # Number of cores for the job. Same as SBATCH -n
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
-#SBATCH -e ./zz.out/amr-%j.err      # Error file for this job.
-#SBATCH -o ./zz.out/amr-%j.out      # Output file for this job.
+#SBATCH -e ./zz.out/bakta-%j.err    # Error file for this job.
+#SBATCH -o ./zz.out/bakta-%j.out    # Output file for this job.
 #SBATCH --account=coa_mki314_uksr   # Project allocation account name (REQUIRED)
 
 if [[ "$1" == "" || "$1" == "-h" ]] ; then
@@ -13,7 +13,7 @@ if [[ "$1" == "" || "$1" == "-h" ]] ; then
    Usage: sbatch ./AMRfinder.bash [folder]
 
    folder      Path to the folder containing the '16.checkm2' directory.
-   
+
    " >&2 ;
    exit 1 ;
 fi ;
@@ -24,8 +24,9 @@ for i in 20.amr_finder ; do
    [[ -d $i ]] || mkdir $i
 done
 
-# Change database path to yours
-database=
+for i in 20.amr_finder/results ; do
+   [[ -d $i ]] || mkdir $i
+done
 
 # Source path to Conda environments
 source /project/mki314_uksr/miniconda3/etc/profile.d/conda.sh
@@ -43,7 +44,8 @@ conda activate amr-finder
 
 for i in $dir/16.checkm2/output/good_quality/*.fa ; do
    name=$(basename "$i" .fa)
-   amrfinder -p test_prot.fa -g test_prot.gff -n test_dna.fa -O Escherichia --plus --database <database_dir> --name <identifier>
+   amrfinder -p $dir/11.bakta/results/$name/$name.faa -g $dir/11.bakta/results/$name/$name.gff3 \
+   | -n $dir/11.bakta/results/$name/$name.fna --plus --annotation_format bakta --name $name
 done ;
 
 conda deactivate
